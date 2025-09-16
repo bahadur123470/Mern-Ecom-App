2160c02833ca565e863a58d9261ed564b509202b
Bahadur Ali <122412801+bahadur123470@users.noreply.github.com>
2025-09-16 18:14:32 +0500

    feat: implement payment method selection and order placement functionality in PlaceOrder component

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import Title from '../components/Title'
-import CartTotal from '../components/CartTotal'
+import React, { useContext, useState } from 'react'
+import Title from '../components/Title.jsx'
+import CartTotal from '../components/CartTotal.jsx'
+import { assets } from '../assets/assets.js'
+import { ShopContext } from '../context/ShopContext.jsx'
 
 const PlaceOrder = () => {
+
+  const [method, setMethod] = useState('cod')
+  const { navigate } = useContext(ShopContext)
+
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
 
@@ -41,7 +47,26 @@ const PlaceOrder = () => {
         </div>
         <div className='mt-12'>
           <Title text1={'PAYMENT'} text2={'METHODS'} />
-          <div className='flex gap-3 flex-col  lg:flex-row'></div>
+          {/* Payment Method Selection */}
+          <div className='flex gap-3 flex-col  lg:flex-row'>
+            <div onClick={()=>setMethod("stripe")} className='flex items-center gap-3  border p-2 px-3 cursor pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === "stripe" ? "bg-green-500" : ""}`}></p>
+              <img src={assets.stripe_logo} className='h-5 mx-4' alt="" />
+            </div>
+            <div onClick={()=>setMethod("razorpay")} className='flex items-center gap-3  border p-2 px-3 cursor pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === "razorpay" ? "bg-green-500" : ""}`}></p>
+              <img src={assets.razorpay_logo} className='h-5 mx-4' alt="" />
+            </div>
+            <div onClick={()=>setMethod("cod")} className='flex items-center gap-3  border p-2 px-3 cursor pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === "cod" ? "bg-green-500" : ""}`}></p>
+              <p className='text-gray-500 text-sm font-medium'>CASH ON DELIVERY</p>
+            </div>
+          </div>
+
+          <div className='w-full text-end mt-8'>
+            <button onClick={()=> navigate("/orders")} className='bbg-black text-white px-16 py-3 text-sm'>PLACE ORDER</button>
+          </div>
+
         </div>
 
       </div>
