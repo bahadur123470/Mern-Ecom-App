7ed8168c6a13735bc797e08b08ae9600fc825d2d
Bahadur Ali <122412801+bahadur123470@users.noreply.github.com>
2025-09-26 14:03:15 +0500

    feat: implement login functionality and update App component for authentication flow

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add.jsx'
 import List from './pages/List.jsx'
 import Order from './pages/Order.jsx'
+import Login from './components/Login.jsx'
+ import { ToastContainer } from 'react-toastify';
 
+export const backendURL = import.meta.env.VITE_BACKEND_URL;
 const App = () => {
+
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
+
+  useEffect(() => {
+    localStorage.setItem('token', token)
+  },[token])
+
   return (
     <div className='bg-gray-50 min-h-screen'>
-      <>
-      <Navbar/>
-      <hr />
-      <div className='flex w-full'>
-        <Sidebar/>
-        <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base'>
-          <Routes>
-            <Route path='/add' element={<Add/>} />
-            <Route path='/list' element={<List/>} />
-            <Route path='/orders' element={<Order/>} />
-          </Routes>
-        </div>
-      </div>
-      </>
+      <ToastContainer />
+      {
+        token === ''
+        ? <Login  setToken={setToken}/>
+        : <>
+          <Navbar setToken={setToken}/>
+          <hr />
+          <div className='flex w-full'>
+            <Sidebar />
+            <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base'>
+              <Routes>
+                <Route path='/add' element={<Add token={token}/>} />
+                <Route path='/list' element={<List token={token}/>} />
+                <Route path='/orders' element={<Order token={token}/>} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      }
     </div>
   )
 }
